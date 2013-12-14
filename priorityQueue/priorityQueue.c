73b281b45ce7f3194a0d79f82e5e45cfff95c13e
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2013-12-14 14:43:23 +0530

    refactored priority queue

--- priorityQueue/priorityQueue.c
+++ priorityQueue/priorityQueue.c
@@ -1,5 +1,7 @@
 #include "priorityQueue.h"
 
+
+
 list* create(){
 	list* listPtr = (list*)calloc(sizeof(list),1);
 	return listPtr;
@@ -9,33 +11,42 @@ void createNode(node *temp,void* data,int priority){
 	temp->priority = priority;
 };
 
+int createFirstNode(node* nodePtr,list* listPtr,void* data,int priority){
+	createNode(nodePtr,data,priority);
+	listPtr->head = nodePtr;
+	listPtr->length +=1;
+	return 1;
+}
+
+int insertFirst(node* nodePtr,node* temp,list* listPtr){
+	nodePtr->next = temp;
+	listPtr->head = nodePtr;
+	listPtr->length +=1;
+	return 1;
+}
+
+int insertLast(node* temp,node* nodePtr,list* listPtr){
+	temp->next = nodePtr;
+	listPtr->length +=1;
+	return 1;	
+}
+
 int enqueue(list* listPtr,int priority,void* data){
 	node* nodePtr = calloc(sizeof(node),1);
 	node *temp=calloc(sizeof(node),1);
 	node *temp2=calloc(sizeof(node),1);
 	if(listPtr->length == 0){
-		createNode(nodePtr,data,priority);
-		listPtr->head = nodePtr;
-		listPtr->length +=1;
-		return 1;	
+		return createFirstNode(nodePtr,listPtr,data,priority);
 	}
 	else{
-		int count = 1;
 		createNode(nodePtr,data,priority);
 		temp = listPtr->head;
-		if(temp->priority > nodePtr->priority){
-			nodePtr->next = temp;
-			listPtr->head = nodePtr;
-			listPtr->length +=1;
-			return 1;	
-		}
+		if(temp->priority > nodePtr->priority)
+			return insertFirst(nodePtr,temp,listPtr);
 		while(temp->next!=NULL)
 			temp= temp->next;	
-		if(temp->priority < nodePtr->priority){
-			temp->next = nodePtr;
-			listPtr->length +=1;
-			return 1;	
-		}
+		if(temp->priority < nodePtr->priority)
+			return insertLast(temp,nodePtr,listPtr);
 		temp = listPtr->head;
 		while(temp->priority < nodePtr->priority){
 			temp2=temp;
@@ -44,7 +55,7 @@ int enqueue(list* listPtr,int priority,void* data){
 		temp2->next = nodePtr;
 		nodePtr->next=temp;
 		listPtr->length +=1;
-			return 1;	
+		return 1;	
 	};
 }
 
@@ -57,3 +68,4 @@ int dequeue(list* listPtr){
 	listPtr->length -=1;
 		return 1;
 }
+
