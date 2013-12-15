eb343079e3dde581eb27c40057e54acf7705b0a1
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2013-12-15 18:51:46 +0530

    insertion sort completed

---
+++ insertionSort/insertionSortTest.c
@@ -0,0 +1,104 @@
+#include "testUtils.h"
+#include "insertionSort.h"
+
+//create setup, tearDown, fixtureSetup, fixtureTearDown methods if needed
+
+
+int compareAccountsByAccountNumber(void* first, void* second) {
+	Account* firstAccount = (Account*)first;
+	Account* secondAccount = (Account*)second;
+	return firstAccount->accountNumber - secondAccount->accountNumber;
+}
+
+int compareAccountsByBalance(void* first, void* second) {
+	Account* firstAccount = (Account*)first;
+	Account* secondAccount = (Account*)second;
+	return firstAccount->balance - secondAccount->balance;
+}
+
+int compareAccountsByName(void* first, void* second) {
+	Account* firstAccount = (Account*)first;
+	Account* secondAccount = (Account*)second;
+	return firstAccount->name - secondAccount->name;
+}
+
+
+int compareInt (void* first, void* second){
+        return (*(int*)first - *(int*)second);
+}
+
+int compareFloat(void* first, void* second){
+		printf("float value ==%f \n",*(float*)first);
+        return (*(float*)first - *(float*)second);
+}
+
+int compareDouble(void* first, void* second){
+        return (*(double*)first - *(double*)second);
+}
+
+int compareChar(void* first, void* second){
+        return (*(char*)first - *(char*)second);
+}
+
+int compareString(void* first, void* second){
+        return strcmp((char*)first, (char*)second);
+}
+
+
+void test_should_arrange_data_in_order_by_Account_Number(){
+	Account account[2] = {{"a",500,12.4},{"b",200,13.6}};;
+	isort(account, 2, sizeof(Account), compareAccountsByAccountNumber);
+	ASSERT(account[0].accountNumber==200);
+	ASSERT(account[1].accountNumber==500);			
+}
+
+void test_should_arrange_data_in_order_by_Account_balance(){
+	Account account[2] = {{"a",500,123.4},{"b",200,13.6}};;
+	isort(account, 2, sizeof(Account), compareAccountsByAccountNumber);
+	ASSERT(account[0].balance==13.6f);
+	ASSERT(account[1].balance==123.4f);			
+}
+
+void test_should_arrange_data_in_order_by_Account_name(){
+	Account account[2] = {{"b",500,123.4},{"a",200,13.6}};;
+	isort(account, 2, sizeof(Account), compareAccountsByAccountNumber);
+	ASSERT(strcmp(account[0].name,"a")==0);
+	ASSERT(strcmp(account[1].name,"b")==0);		
+}
+ 
+void test_should_arrange_int_data_in_order(){
+	int data[5] = {5,4,3,2,1};
+    int expected[5] = {1,2,3,4,5};
+    isort(data, 5, sizeof(int), compareInt);
+    ASSERT(data[0]==1);
+    ASSERT(data[4]==5);
+}
+
+void test_should_arrange_double_data_in_order(){
+	double data[5] = {5.1,4.1,3.1,2.1,1.1};
+    double expected[5] = {1.1,2.1,3.1,4.1,5.1};
+    isort(data, 5, sizeof(double), compareDouble);
+    ASSERT(data[0]==1.1);
+    ASSERT(data[4]==5.1);
+}
+
+// void test_should_arrange_float_data_in_order(){
+// 	float data[5] = {5.1,4.12,3.13,2.14,1.15};
+//     isort(data, 5, sizeof(float), compareFloat);
+//     ASSERT(data[0]==1.15f);
+//     ASSERT(data[4]==5.1f);
+// }
+
+void test_should_arrange_char_data_in_order(){
+	char data[5] = {'e','d','c','b','a'};
+    isort(data, 5, sizeof(char), compareChar);
+    ASSERT(data[0]=='a');
+    ASSERT(data[4]=='e');
+}
+
+void test_should_arrange_string_data_in_order(){
+	string data[2] = {"manish","amit"};
+	isort(data,2,sizeof(string),compareString);
+	ASSERT(strcmp(data[0],"amit")==0);
+	ASSERT(strcmp(data[1],"manish")==0);
+}
---
+++ insertionSort/insertionSortTestRunner.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+
+int testCount=-1;
+int passCount=0;
+void setup();
+void tearDown();
+
+void fixtureSetup();
+void fixtureTearDown();
+void incrementTestCount();
+void incrementPassCount();
+int currentTestFailed=0;
+
+void testStarted(char* name){
+	incrementTestCount();
+	currentTestFailed=0;
+	printf("\t%s\n",name);
+}
+
+void testEnded(){
+	if(!currentTestFailed)
+		incrementPassCount();
+}
+
+void resetTestCount(){
+	testCount=0;
+	passCount=0;
+	printf("********* Starting tests\n\n");
+}
+
+void summarizeTestCount(){
+	printf("\n********* Ran %d tests passed %d failed %d\n",testCount,passCount,testCount-passCount);
+}
+
+void incrementTestCount(){
+	testCount++;
+}
+
+void incrementPassCount(){
+	passCount++;
+}
+
+void testFailed(const char* fileName, int lineNumber, char* expression){
+	currentTestFailed = 1;
+	printf("\t\t %s : failed at %s:%d\n",expression, fileName,lineNumber);
+}
+
+int main(){
+	fixtureSetup();
+	resetTestCount();
+
+	testStarted("test_should_arrange_data_in_order_by_Account_Number");
+	setup();
+		test_should_arrange_data_in_order_by_Account_Number();
+	tearDown();
+	testEnded();
+	testStarted("test_should_arrange_data_in_order_by_Account_balance");
+	setup();
+		test_should_arrange_data_in_order_by_Account_balance();
+	tearDown();
+	testEnded();
+	testStarted("test_should_arrange_data_in_order_by_Account_name");
+	setup();
+		test_should_arrange_data_in_order_by_Account_name();
+	tearDown();
+	testEnded();
+	testStarted("test_should_arrange_int_data_in_order");
+	setup();
+		test_should_arrange_int_data_in_order();
+	tearDown();
+	testEnded();
+	testStarted("test_should_arrange_double_data_in_order");
+	setup();
+		test_should_arrange_double_data_in_order();
+	tearDown();
+	testEnded();
+	testStarted("test_should_arrange_char_data_in_order");
+	setup();
+		test_should_arrange_char_data_in_order();
+	tearDown();
+	testEnded();
+	testStarted("test_should_arrange_string_data_in_order");
+	setup();
+		test_should_arrange_string_data_in_order();
+	tearDown();
+	testEnded();
+
+	summarizeTestCount();
+	fixtureTearDown();
+	return 0;
+}
+
+void setup(){}
+
+void tearDown(){}
+
+void fixtureSetup(){}
+
+void fixtureTearDown(){}
