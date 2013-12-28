875b4289fd8523030c3caebdd3431e28f25fcc7c
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2013-12-28 18:19:02 +0530

    made all compare functions

--- bubble-sort/bubbleSortTest.c
+++ bubble-sort/bubbleSortTest.c
@@ -1,8 +1,25 @@
 #include "testUtils.h"
 #include "bubbleSort.h"
+#include <string.h>
 
 //create setup, tearDown, fixtureSetup, fixtureTearDown methods if needed
 
-void test_fail(){
-	ASSERT(0);
+int compareInt (void* first, void* second){
+        return (*(int*)first - *(int*)second);
+}
+
+int compareFloat(void* first, void* second){
+        return (*(float*)first - *(float*)second);
+}
+
+int compareDouble(void* first, void* second){
+        return (*(double*)first - *(double*)second);
+}
+
+int compareChar(void* first, void* second){
+        return (*(char*)first - *(char*)second);
+}
+
+int compareString(void* first, void* second){
+        return strcmp((char*)first, (char*)second);
 }
