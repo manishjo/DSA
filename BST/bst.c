9af5649297a23f74171801083bc3edfea4a5b975
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2014-01-01 22:45:56 +0530

    create function is completed

--- BST/bst.c
+++ BST/bst.c
@@ -1,7 +1,15 @@
 #include "bst.h"
+#include <stdlib.h>
 
 typedef struct node{
 	void* data;
 	struct node* leftChild;
 	struct node* rightChild;
 }Bst_node;
+
+Bst* create(compareFunc* compareFunction){
+	Bst* tree = calloc(1,sizeof(Bst));
+	tree->compare = compareFunction;
+	tree->root = NULL;
+	return tree;
+}
--- BST/bstTest.c
+++ BST/bstTest.c
@@ -1,5 +1,6 @@
 #include "testUtils.h"
 #include "bst.h"
+#include <stdlib.h>
 
 //create setup, tearDown, fixtureSetup, fixtureTearDown methods if needed
 
@@ -9,4 +10,9 @@ int compareIntData(void*first, void*second){
 
 int compareCharData(void*first, void*second){
 	return *(char*)first == *(char*)second;
+}
+
+void test_create_should_create_a_tree(){
+	Bst* tree = create(compareIntData);
+	ASSERT(tree->root == NULL);	
 }
