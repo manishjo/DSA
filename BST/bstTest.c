8430fad0b13071cb3f5eff969f2553ddd4aae749
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2014-01-01 19:15:08 +0530

    made compare function

--- BST/bstTest.c
+++ BST/bstTest.c
@@ -3,3 +3,10 @@
 
 //create setup, tearDown, fixtureSetup, fixtureTearDown methods if needed
 
+int compareIntData(void*first, void*second){
+	return *(int*)first == *(int*)second;
+}
+
+int compareCharData(void*first, void*second){
+	return *(char*)first == *(char*)second;
+}
